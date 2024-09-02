d9c56a3096aa67e4bf8e28089f0c5af575e474f2
ageria20 <ageria20@gmail.com>
2024-09-02 16:36:42 +0200

    Initial commit

---
+++ U5s5l1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ageria.u5s5l1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5s5l1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
